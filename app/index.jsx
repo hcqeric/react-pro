f82b3ebc48ea6d7db52e14baafdd599e09380219
mxlk <hcqeric@126.com>
2018-03-25 18:58:32 +0800

    react basic usage

--- app/index.jsx
+++ app/index.jsx
@@ -5,10 +5,31 @@ import './static/css/common.less'
 
 class Hello extends React.Component {
     render() {
+      let m = <p>hello world11111vadv</p>
+      let arr = ["sdfasdf","dsfasdf","adsfasdf"]
         return (
-            <p>hello world11111vadv</p>
+          <div>
+            {m}
+            <p style={{"fontSize":"50px","color":"red"}}  className="p1" onClick={this.onHandleClick.bind(this)}>hello world11111vadv</p>
+            <ul>
+              {arr.map((item, index)=>{
+                return <li key={index}>{item}</li>
+              })}
+            </ul>
+            {
+              false?<p>true</p>:
+              <div>
+                <p>dsfasd</p>
+                <p>dsfasd</p>
+              </div>
+            }
+          </div>
         )
     }
+    onHandleClick(){
+      alert("asfasdf")
+    }
+
 }
 
 render(
